4e27a52f0775c72d43d5b1fbae993255e0c90238
borkoloco <santiagobosca@gmail.com>
2024-09-09 17:11:27 -0300

    chat

--- varios/React-socket-chat-app-1.13.0/src/types/types.js
+++
@@ -1,9 +0,0 @@
-
-
-export const types = {
-
-    usuariosCargados: '[Chat] Usuarios cargados',
-    activarChat: '[Chat] Activar Chat',
-    nuevoMensaje: '[Chat] Nuevo Mensaje',
-    cargarMensajes: '[Chat] Cargar Mensajes'
-}
