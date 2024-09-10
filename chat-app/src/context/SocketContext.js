9ed953211731e4511d2d349de8998da53df308a9
borkoloco <santiagobosca@gmail.com>
2024-09-10 12:34:09 -0300

    url

--- chat-app/src/context/SocketContext.js
+++ chat-app/src/context/SocketContext.js
@@ -10,7 +10,7 @@ export const SocketContext = createContext();
 
 export const SocketProvider = ({ children }) => {
   const { socket, online, conectarSocket, desconectarSocket } = useSocket(
-    "http://localhost:8080"
+    process.env.REACT_APP_BACK_URL
   );
   const { auth } = useContext(AuthContext);
   const { dispatch } = useContext(ChatContext);
